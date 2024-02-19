4bf1e14c25f2fa858ffbf5ba765b2a58b701dc0d
Leandro Rodrigues <lj251004@gmail.com>
2024-02-19 19:18:03 -0300

    fixed error with search page

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,27 +1,17 @@
-"use client";
-
-import { redirect, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 import { SearchFeed } from "@/components/search/search-feed";
 
 interface PageProps {}
 
 export default function Page({}: PageProps) {
-  const searchParams = useSearchParams();
-  const query = searchParams.get("q");
-
-  if (!query) redirect("/");
-
   return (
     <div className="flex flex-col w-full gap-y-2 px-6 py-4 overflow-y-auto relative">
       <h1 className="text-xl font-semibold">Buscar</h1>
 
-      <p className="text-muted-foreground">
-        Exibindo resultados para:{" "}
-        <span className="text-lg font-semibold">{query}</span>
-      </p>
-
-      <SearchFeed query={query} />
+      <Suspense fallback={<div>Carregando...</div>}>
+        <SearchFeed />
+      </Suspense>
     </div>
   );
 }
--- components/search/search-feed.tsx
+++ components/search/search-feed.tsx
@@ -2,18 +2,25 @@
 
 import { sidebarLinks } from "@/links/sidebar";
 import { MainCard } from "@/components/ui/main-card";
+import { useSearchParams } from "next/navigation";
 
-interface SearchFeedProps {
-  query: string;
-}
+interface SearchFeedProps {}
 
-export function SearchFeed({ query }: SearchFeedProps) {
+export function SearchFeed({}: SearchFeedProps) {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("q");
+
   const data = sidebarLinks.filter((link) =>
     link.title.toLowerCase().includes(query?.toLowerCase()!!)
   );
 
   return (
     <>
+      <p className="text-muted-foreground">
+        Exibindo resultados para:{" "}
+        <span className="text-lg font-semibold">{query}</span>
+      </p>
+      
       {data.length ? (
         <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-4 pb-20">
           {data.map((link) => (
